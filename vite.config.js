dd98c8976ca0862290dd332daf90a274cf266c90
ssumir <145274759+ssumir@users.noreply.github.com>
2025-09-25 13:50:01 +0600

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cms/', // ✅ required for GitHub Pages deployment
   server: {
     proxy: {
       '/api': {
